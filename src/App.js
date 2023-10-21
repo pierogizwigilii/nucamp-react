1abac720d88fde5870de4f8e9251521bfb1cc10a
Martyna Runkowska <martyna@cosy.land>
2023-10-21 16:24:20 +0100

    Commit message

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import CampsitesList from './features/campsites/CampsitesList';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <CampsitesList />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
