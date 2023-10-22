3b9ec06ac69c76c006b4f40f5a3a2ceafb3f7f6e
Martyna Runkowska <martyna@cosy.land>
2023-10-22 16:36:56 +0100

    state, hooks, react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import './App.css';
-import CampsitesList from './features/campsites/CampsitesList';
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <CampsitesList />
-      <Footer />
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Header />
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='contact' element={<ContactPage />} />
+                <Route path='directory' element={<CampsitesDirectoryPage />} />
+                <Route
+                    path='directory/:campsiteId'
+                    element={<CampsiteDetailPage />}
+                />
+            </Routes>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/features/campsites/campsitesSlice.js
@@ -0,0 +1,18 @@
+import { CAMPSITES } from '../../app/shared/CAMPSITES';
+
+export const selectAllCampsites = () => {
+    return CAMPSITES;
+};
+
+export const selectCampsiteById = (id) => {
+    return CAMPSITES.find((campsite) => campsite.id === parseInt(id));
+};
+
+export const selectFeaturedCampsite = () => {
+    return CAMPSITES.find((campsite) => campsite.featured);
+};
+
+//placeholder code for randomly selecting campsite, but not needed anymore
+// export const selectRandomCampsite = () => {
+//     return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
+// };
